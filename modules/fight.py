ac86a28770a1c23e66bfb035f68bdfe2545a221f
Jacob Holtom <jacob@holtom.me>
2021-03-25 18:20:55 +0000

    Add rudimentary statistics tracking

--- modules/fight.py
+++ modules/fight.py
@@ -18,10 +18,15 @@ def fight(bot, trigger):
         out = "gimme something for you to fight with"
     else:
         fighter2 = trigger.group(2)
+        # Keep track of the statistics for fighting
+        curr_wins = bot.db.get_nick_value(fighter1,'fight_wins',0)
+        opponent_wins = bot.db.get_nick_value(fighter2,'fight_wins',0)
+        
         if random.random() < .5:
-            out = "{}! {}! {}! {} {} over {} with a {} {}.".format(random.choice(bang), random.choice(bang), random.choice(bang), fighter1, random.choice(victory),fighter2, random.choice(blow_type), random.choice(blow))
+            bot.db.set_nick_value(fighter1,'fight_wins',curr_wins+1)
+            out = "{}! {}! {}! {} {} over {} with a {} {}.".format(random.choice(bang), random.choice(bang), random.choice(bang), fighter1, random.choice(victory),fighter2, random.choice(blow_type), random.choice(blow))            
         else:
+            bot.db.set_nick_value(fighter2,'fight_wins',opponent_wins+1)
             out = "{}! {}! {}! {} {} over {} with a {} {}.".format(random.choice(bang), random.choice(bang), random.choice(bang), fighter2, random.choice(victory),fighter1, random.choice(blow_type), random.choice(blow))
-
+            
     return bot.say(out)
-    
